3d0ea3309aa4e037f2e1e54c7066008bb4af4274
Luciano Recchini <lucianorecchini@FBZ-MAC-003.local>
2021-03-20 21:55:12 -0300

    drawer new

---
+++ src/components/Drawer.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
+import Button from '@material-ui/core/Button'
+import MenuIcon from '@material-ui/icons/Menu'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        background: theme.palette.secondary.dark,
+        height: '100vh'
+    },
+    list: {
+        width: 250,
+    },
+    fullList: {
+        width: 'auto',
+    },
+}));
+
+export default function SwipeableTemporaryDrawer({children}) {
+
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  return (
+    <div>
+      {['right'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}><MenuIcon /></Button>
+          <SwipeableDrawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+            onOpen={toggleDrawer(anchor, true)}
+          >
+            <div className={classes.root}>
+            {children}
+            </div>
+          </SwipeableDrawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import Divider from '@material-ui/core/Divider'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import Avatar from '@material-ui/core/Avatar'
+
 import { useHistory, Link } from 'react-router-dom'
 import dayjs from 'dayjs'
 
@@ -7,7 +13,11 @@ import SimpleImage from './SimpleImage'
 import LogoAlamar from '../assets/alamar-logo-2.svg'
 import Typography from './Typography'
 
-import IconPlaya from '../assets/icon-playa.svg'
+import SwipeableTemporaryDrawer from './Drawer'
+
+import IconPlaya from '../assets/icon-playa2.svg'
+import Accepted from '../assets/accepted.svg'
+import Conversation from '../assets/conversation.svg'
 
 const useStyles = makeStyles(theme => ({
   header: {
@@ -40,6 +50,10 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
+
+    '& svg': {
+      color: 'white',
+    },
   },
   contentLogo: {
     cursor: 'pointer',
@@ -52,13 +66,21 @@ const useStyles = makeStyles(theme => ({
   },
   nav: {
     display: 'flex',
+    flexDirection: 'column',
     listStyle: 'none',
     color: 'black',
     alignItems: 'center',
-    padding: 0,
+    padding: 10,
 
     '& li': {
-      margin: '0 10px',
+      margin: 5,
+      padding: 0,
+      width: '100%',
+      color: 'white',
+
+      '& hr': {
+        background: 'white',
+      },
 
       '& a': {
         textDecoration: 'none',
@@ -66,7 +88,7 @@ const useStyles = makeStyles(theme => ({
         alignItems: 'center',
 
         '& img': {
-          marginRight: 5
+          marginRight: 15
         },
 
         '&:hover': {
@@ -92,23 +114,60 @@ const Header = () => {
             <SimpleImage alt="Alamar - Balnearios Costa Atlantica" height={30} alt='Alamar' image={LogoAlamar} onClick={() => history.push('/')} />
           </Link>
         </div>
-        <ul className={classes.nav}>
-          <li>
-            <Link to={`/list/${dia}/${dia}`}>
-              <SimpleImage alt="Alquilar Balneario Costa Atlantica" height={30} image={IconPlaya} />
-              <Typography variant='p' color='white' fontWeight={400}>
-                Balnearios
-              </Typography>
-            </Link>
-          </li>
-          {/* <li>
-            <Link to='/'>
-              <Typography variant='p' color='white'>
-                Ayuda
-              </Typography>
-            </Link>
-          </li> */}
-        </ul>
+        <SwipeableTemporaryDrawer>
+          <ul className={classes.nav}>
+            {/* <ListItem alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar alt="Luiciano Recchini" src="/static/images/avatar/1.jpg" />
+              </ListItemAvatar>
+              <ListItemText
+                primary="Biuenvenido"
+                secondary={
+                  <React.Fragment>
+                    <Typography
+                      component="span"
+                      variant="body2"
+                      className={classes.inline}
+                      color="white"
+                    >
+                      Luciano Recchini
+                    </Typography>
+                  </React.Fragment>
+                }
+              />
+            </ListItem> */}
+            <li>
+              <Link to={`/list/${dia}/${dia}`}>
+                <SimpleImage alt="Alquilar Balneario Costa Atlantica" height={28} image={IconPlaya} />
+                <Typography variant='p' textAlign='left' color='white' fontWeight={400}>
+                  Balnearios
+                </Typography>
+              </Link>
+            </li>
+            <li>
+              <Divider />
+            </li>
+            <li>
+              <Link to='/'>
+                <SimpleImage alt="Alquilar Balneario Costa Atlantica" height={28} image={Accepted} />
+                <Typography variant='p' color='white'>
+                  Terminos y Condiciones
+                </Typography>
+              </Link>
+            </li>
+            <li>
+              <Divider />
+            </li>
+            <li>
+              <Link to='/'>
+                <SimpleImage alt="Alquilar Balneario Costa Atlantica" height={28} image={Conversation} />
+                <Typography variant='p' color='white'>
+                  Preguntas Frecuentes
+                </Typography>
+              </Link>
+            </li>
+          </ul>
+        </SwipeableTemporaryDrawer>
       </div>
     </div>
   )
